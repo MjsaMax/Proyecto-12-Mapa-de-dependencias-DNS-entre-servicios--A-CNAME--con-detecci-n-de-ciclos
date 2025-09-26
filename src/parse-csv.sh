fd7cf0f12c33320b6732a1506cf1be8d0ec53ed6
Walter <walter.poma.n@uni.pe>
2025-09-26 01:12:39 -0500

    Inicializar script de parseo y estructura para grafo DNS

---
+++ src/parse-csv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# --- Variables de configuración ---
+INPUT_FILE="out/dns-resolved.csv"
+OUTPUT_FILE="out/edge-list.txt"
+
+## --- Sección para luego implementar el script--- 
+main(){
+
+    echo "Iniciando el script para parsear el CSV"
+
+}
+
+## LLamada a la función para ejecutar el script
+main
